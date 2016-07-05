ae35f3d2c95c82332abb49f5f04feadb81d72103
Jose Servet <frizhard@gmail.com>
2016-07-05 19:10:25 +0200

    Initial Commit

---
+++ SWCraftAL-KataRomanNumerals2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SWCraftAL-KataRomanNumerals2
+//
+//  Created by José Servet Font on 5/7/16.
+//  Copyright © 2016 ByBDesigns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
